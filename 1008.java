00259d2a555bd142e7c84d4a523d4c79bc0c1121
akalswl14 <42285463+akalswl14@users.noreply.github.com>
2019-01-02 16:59:30 +0900

    Create 1008.java

---
+++ 1008.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		double x = s.nextDouble()/s.nextDouble();
+		System.out.println(x);
+		s.close();
+	}
+
+}
