30fbf29035ba5ac88f5fc6fabf03f3ab2cfd4081
akalswl14 <42285463+akalswl14@users.noreply.github.com>
2019-01-02 16:59:52 +0900

    Create 10869.java

---
+++ 10869.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int x = s.nextInt();
+		int y = s.nextInt();
+		System.out.println(x+y);
+		System.out.println(x-y);
+		System.out.println(x*y);
+		System.out.println(x/y);
+		System.out.println(x%y);
+		s.close();
+	}
+
+}
