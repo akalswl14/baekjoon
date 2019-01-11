e21554cc6554dd21a5fa2ec2a68f20be1ba5b172
akalswl14 <42285463+akalswl14@users.noreply.github.com>
2019-01-12 03:40:35 +0900

    Update 1924.java

--- 1924.java
+++ 1924.java
@@ -1,48 +1,46 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Main {
+	static StringTokenizer st;
+	static int x, y;
 
-	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		int month = s.nextInt();
-		int day = s.nextInt();
-		int sum = 0;
-		for(int i=1;i<month;i++) {
-			if(i==4||i==6||i==9||i==11) {
-				sum+=30;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		st = new StringTokenizer(br.readLine());
+		x = Integer.parseInt(st.nextToken());
+		y = Integer.parseInt(st.nextToken());
+		for (int i = 1; i < x; i++) {
+			if (i == 1 || i == 3 || i == 5 || i == 7 || i == 8 || i == 10 || i == 12) {
+				y += 31;
+			} else if (i == 4 || i == 6 || i == 9 || i == 11) {
+				y += 30;
+			} else {
+				y += 28;
 			}
-			else if(i==2) {
-				sum+=28;
-			}
-			else {
-				sum+=31;
-			}
 		}
-		sum+=day;
-		sum%=7;
-		if(sum==1) {
-			System.out.println("MON");
+		if (y % 7 == 1) {
+			bw.write("MON");
+		} else if (y % 7 == 2) {
+			bw.write("TUE");
+		} else if (y % 7 == 3) {
+			bw.write("WED");
+		} else if (y % 7 == 4) {
+			bw.write("THU");
+		} else if (y % 7 == 5) {
+			bw.write("FRI");
+		} else if (y % 7 == 6) {
+			bw.write("SAT");
+		} else {
+			bw.write("SUN");
 		}
-		else if(sum==2) {
-			System.out.println("TUE");
-		}
-		else if(sum==3) {
-			System.out.println("WED");
-		}
-		else if(sum==4) {
-			System.out.println("THU");
-		}
-		else if(sum==5) {
-			System.out.println("FRI");
-		}
-		else if(sum==6) {
-			System.out.println("SAT");
-		}
-		else{
-			System.out.println("SUN");
-		}
-		
-		s.close();
+		bw.flush();
+		bw.close();
+		return;
 	}
-
 }
